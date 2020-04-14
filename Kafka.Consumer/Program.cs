d29454fb010bc79c0242b6423991b23e700a8b51
intersect88 <intersect@anche.no>
2020-04-14 11:21:54 +0200

    Fix previous commit

---
+++ Kafka.Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Confluent.Kafka;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var conf = new ConsumerConfig
+        {
+            GroupId = "demo-consumer-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(conf).Build())
+        {
+            consumer.Subscribe("demo-topic");
+
+            var readingMessage = true;
+            try
+            {
+                while (readingMessage)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume(10000);
+                        if (consumeResult == null)
+                        {
+                            Console.WriteLine("There are no messages in topic");
+                            readingMessage = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Consumed message '{consumeResult.Message.Value}' at: '{consumeResult.TopicPartitionOffset}'.");
+                        }
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Kafka.Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092"};
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        var deliveryResult = await producer.ProduceAsync("demo-topic", new Message<Null, string> { Value = $"DemoMessage '{i}'" });
+                        Console.WriteLine($"Published message to '{deliveryResult.TopicPartitionOffset}'");
+                    }
+                    
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Error publishing message: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
